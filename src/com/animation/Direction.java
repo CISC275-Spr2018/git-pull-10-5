87e6494f16c3bc83ba0eeea4732fa57f6ed10873
Chen Dai <chendai@udel.edu>
2018-04-15 20:32:25 -0400

    1.1

---
+++ src/com/animation/Direction.java
@@ -0,0 +1,23 @@
+package com.animation;
+
+public enum Direction {
+
+	NORTH("north"),
+	NORTHEAST("northeast"),
+	EAST("east"),
+	SOUTHEAST("southeast"),
+	SOUTH("south"),
+	SOUTHWEST("southwest"),
+	WEST("west"),
+	NORTHWEST("northwest");
+	
+	private String name = null;
+	
+	private Direction(String s){
+		name = s;
+	}
+	public String getName() {
+		return name;
+	}
+
+}
